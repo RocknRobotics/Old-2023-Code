ccdffd46bca7d095a9ca47d8eb5d48e7e829f639
KDu0ng <125706266+KDu0ng@users.noreply.github.com>
2023-02-20 17:45:46 -0600

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,24 @@
+public class Constants
+{
+    public static final double RampUp = 2;
+
+    final double accelProportion = 1.0;
+    //Time that it takes the robot to drive back to a new piece
+    final double autoBackUpTime = 0.0;
+    //Time that it takes the robot to drive back to a new piece from Station 2
+    final double autoBackUpTime2 = 0.0;
+    //Time for the robot to turn around so it can grab a piece---Should be the same for all stations
+    final double turnTime =  0.0;
+    //Time for robot to orient towards the teeter totter
+    final double teeterTurnTime = 0.0;
+    //Time for the robot to drive backwards towards the teeter totter
+    final double teeterDriveTime = 0.0;
+    //Station 2 code
+    final double teeterDriveTime2 = 0.0;
+    //Time stamp for the robot to line up to not take up all the space on the teeter totter
+    final double teeterOrientTime = 0.0;
+    //Time to stay on the teeter
+    final double onTeeterTime = 0.0;
+    //Postion that the robot must be at on the teeter totter for it to be stable, assuming it starts just in front of the teeter totter.
+    final double teeterPosition = 0.0;
+}
