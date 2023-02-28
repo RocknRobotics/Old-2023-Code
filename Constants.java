58b372ddfbd1fc4481931979c7b9059dd5beb3e5
NoahZeNairwhal <122690215+NoahZeNairwhal@users.noreply.github.com>
2023-02-28 08:23:34 -0600

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -4,7 +4,13 @@ public class Constants
 {
     public static final double RampUp = 2;
 
-    public static boolean blueAlliance = true;
+    public static final boolean blueAlliance = true;
+
+    //Milliseconds, need to find actual values
+    public static final double driveOutTime = 100;
+    public static final double spinTime = 100;
+    public static final double overTeeterTime = 100;
+    public static final double onTeeterTime = 100;
 
     public static final double armAngleTolerance = 0.0;
     public static final double armLengthTolerance = 0.0;
